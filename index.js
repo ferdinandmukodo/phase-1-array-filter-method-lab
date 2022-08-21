286024fdf2f962c08caf8b5048cdbd8303f4c46d
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-08-21 11:47:07 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby']
+
+const findMatching = (parameter1,parameter2) => parameter1.filter(parameter1 => parameter1.toLowerCase() === parameter2.toLowerCase())
+
+const fuzzyMatch = (parameter1,parameter2) => parameter1.filter(parameter1 => parameter1.charAt(0) === parameter2.charAt(0))
+
+const matchName = (parameter1,parameter2) => parameter1.filter(parameter1 => parameter1.name === parameter2)
